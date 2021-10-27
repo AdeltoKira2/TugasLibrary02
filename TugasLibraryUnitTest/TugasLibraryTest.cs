6f13a114835bc564f018c701e78ac9a887628520
AdeltoKira2 <91925416+AdeltoKira2@users.noreply.github.com>
2021-10-27 14:55:53 +0700

    Add files via upload

---
+++ TugasLibraryUnitTest/TugasLibraryTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    public class TugasLibraryTest
+    {
+        private Day hari;
+        private User pengguna;
+        [SetUp]
+        public void init()
+        {
+            hari = new Day();
+            pengguna = new User();
+        }
+        [Test]
+        public void NameOfTheDayTest()
+        {
+            Assert.AreEqual("Senin", hari.NameOfDay(1));
+            Assert.AreEqual("Selasa", hari.NameOfDay(2));
+            Assert.AreEqual("Rabu", hari.NameOfDay(3));
+            Assert.AreEqual("Kamis", hari.NameOfDay(4));
+            Assert.AreEqual("Jum'at", hari.NameOfDay(5));
+            Assert.AreEqual("Sabtu", hari.NameOfDay(6));
+            Assert.AreEqual("Minggu", hari.NameOfDay(7));
+        }
+        [Test]
+        public void TestUserValid()
+        {
+            Assert.IsTrue(pengguna.IsValidUser("admin", "admin"));
+        }
+    }
+}
